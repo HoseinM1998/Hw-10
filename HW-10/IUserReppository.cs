aa23d70855624071718098aac4fb6032d66ad16e
HosseinM1998 <hmoslehi98@gmail.com>
2024-11-01 10:29:01 +0330

    CreatedatabaseSql

---
+++ HW-10/IUserReppository.cs
@@ -0,0 +1,11 @@
+﻿
+public interface IUserReppository
+{
+    public void Add(User user);
+    public List<User> GetAll();
+    public User Get(string username);
+    public void Update(User user);
+    public void Delete(string userName);
+
+}
+
